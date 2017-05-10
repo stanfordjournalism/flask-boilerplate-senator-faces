3087d9883ccc7faa787fb898648fa40e68da6d70
Dan Nguyen <dansonguyen@gmail.com>
2017-05-10 11:23:05 -0700

    more stuff

--- app.py
+++ app.py
@@ -2,12 +2,23 @@ from flask import Flask
 from flask import render_template
 from helpers.legislators import get_senators, find_senator_by_id
 
+from os.path import exists
+
 app = Flask(__name__)
 
 @app.route("/hello")
 def helloworld():
     return render_template('hello.html')
 
+@app.route('/press-releases/<name_of_press_release>')
+def trump(name_of_press_release):
+    filename = 'helpers/templates/' + name_of_press_release + '.html'
+    if exists(filename):
+        return render_template(name_of_press_release + '.html')
+    else:
+        return """
+        <div class="page-not-found__background-image" style="background-image:url('https://assets.bwbx.io/business/public/images/bbiz404-d662b65a97.gif');"></div>
+        """
 @app.route("/")
 def homepage():
     return render_template('index.html')
